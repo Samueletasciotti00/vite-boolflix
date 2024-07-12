8233d97bfdbc221333b88e2f56b001fe016f96a3
Samuele <samueletasciotti@icloud.com>
2024-07-12 11:04:58 +0200

    to fix

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <script>
+// Import Components
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+    name: 'App',
+    components: {
+      AppHeader
+    },
+    data() {
+      return {
+        
+      }
+    }
+} 
 
 </script>
 
 <template>
-  
+    
+    <!-- Header -->
+    <AppHeader />
+
+    <!-- AppMain -->
 </template>
 
 <style>
-
+    body{
+      background-color: gray;
+    }
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,64 @@
 <script>
 
+import axios from 'axios'
+import MovieList from 'MovieList.vue'
     // Scrivere l'export dell componente
+    export default {
+        name: 'AppHeader',
+        components: {
+            MovieList
+        },
+        data() {
+            return {
+                query:'',
+                movies: [],
+                apiKey: '70b5d4fb18cfb09b8168cc33b8d445d7'
+            };
+        },
+        methods: {
+            async searchMovies() {
+                if (this.query.length > 2 ) {
+                    try {
+                        const response = await axios.get(`https://api.themoviedb.org/3/search/movie`, {
+                            params: {
+                                api_key: this.apiKey,
+                                query: this.query
+                            }
+                        });
+                        this.movies = response.data.results;
+                    } catch (error) {
+                        console.error('Movie Errore', error);
+                    }
+                } else {
+                    this.movie = [];
+                }
+            }
+        }
+    };
 </script>
 
 <template>
-  
+
+    <!-- Html of Component -->
+  <div class="container-fluid bg-black">
+    <div class="row">
+      <div class="col-12">
+        <div></div>
+        <div>
+          <!-- Ricerca Film/serie -->
+          <input type="text" v-model="query" @input="searchMovies" placeholder="ricerca...">
+
+          <!-- Avvia Ricerca -->
+          <button @click="searchMovies">Cerca</button>
+        </div>
+      </div>
+    </div>
+    <MovieList :movies="movies" />
+  </div>
 </template>
 
 <style>
-
+.container-fluid{
+    padding: 20px;
+}
 </style>
