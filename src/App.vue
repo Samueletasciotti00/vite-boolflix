3ffa4d56278a47e1c13b072a83bcc4a391485f10
Samuele <samueletasciotti@icloud.com>
2024-07-13 22:00:17 +0200

    feat: immage for films

--- src/App.vue
+++ src/App.vue
@@ -15,33 +15,53 @@ export default {
     },
     data() {
       return {
-        store,
+        store
       }
     },
     methods: {
-      getFilms(){
+      getInfo(){
 
-        let endPoint = store.searchText ? `${store.apiURL}${store.apiNameParam}${store.searchText}` : store.apiURL; 
-        console.log(endPoint);
-        store.loading = true;
+        let endPoint = store.searchText ? `${store.apiURL}${store.apiNameParam}${store.searchText}` : store.apiURL; // Ricerca film
+
+        let endPointSeries = store.searchText ? `${store.urlSeries}${store.apiNameParam}${store.searchText}` : store.urlSeries; //Ricerca serie
+
+        store.loading = true; //Prima della risposta API vediamo il caricamento in pagina.
         // Gestione delle chiamate con Axios
         axios
-        .get(endPoint)
+        .get(endPoint) //Facciamo richiesta attraverso 'get' e 'endPoint' 
         .then(res => {
-          console.log(res);
-          store.movieList = res.data.results;
-          store.loading = false;
+          store.movieList = res.data.results; //Restituisce l'array dei film trovati nel nostro array vuoto.
+          store.flagConverter = res.data.results.original_language; //Mettiamo nello store la lingua ottenuta.
+          store.loading = false; //Interrompe lo stato loading della richiesta.
         })
         .catch(err => {
-          console.log(err);
-          store.loading = false;
+          console.log(err); //Debug di verifica errore chiamata.
+          store.loading = false; //Interrompe lo stato loading della richiesta.
         });
 
-         
-      }
+        
+        axios // Chiamata API per le Series.
+        .get(endPointSeries) //Facciamo richiesta attraverso 'get' e 'endPoint' 
+        .then(res => {
+          store.seriesList = res.data.results; //Restituisce l'array dei film trovati nel nostro array vuoto.
+          store.flagConverter = res.data.results.original_language; //Mettiamo nello store la lingua ottenuta.
+          store.loading = false; //Interrompe lo stato loading della richiesta.
+        })
+        .catch(err => {
+          console.log(err); //Debug di verifica errore chiamata.
+          store.loading = false; //Interrompe lo stato loading della richiesta.
+        });
+      },
+
+      // ora prendiamo le immagini e stampiamole in pagina
+      getImageUrl(posterPath) {
+          const baseUrl = 'https://image.tmdb.org/t/p/';
+          const size = 'w342'; // You can change the size according to your needs
+          return `${baseUrl}${size}${posterPath}`;
+        }
     },
     created() {
-      this.getFilms();
+      this.getInfo();
     }
 } 
 
@@ -50,19 +70,37 @@ export default {
 <template>
 
     <!-- Header -->
-    <AppHeader @search="getFilms" />
+    <AppHeader @search="getInfo" @keyup.enter="getInfo"/>
     
     <!-- Lista film -->
     <div v-if="store.loading"><h1>Caricamento...</h1></div>
+
+
+    <!-- Elenco film -->
+    <h2>Films</h2>
     <ul>
       <li v-for="movie in store.movieList" :key="movie.id"> <span>TITLE :</span> {{ movie.title }} <span>ORIGINAL_TITLE :</span> {{ movie.original_title }} <span>LENG : </span>
-      {{movie.original_language}} <span>VOTE :</span>  {{movie.vote_average}}</li>
+      <div class="inline" v-html="store.flagger"></div> <span>VOTE :</span>  {{movie.vote_average}} <span> IMG : </span> <img :src="getImageUrl(movie.poster_path)" alt=""></li>
+    </ul>
 
+    <!-- Elenco seires -->
+    <h2>Series</h2>
+    <ul>
+       <li v-for="serie in store.seriesList" :key="serie.id"> <span>NAME :</span> {{ serie.name }} <span>ORIGINAL_NAME :</span> {{ serie.original_name }} <span>LENG : </span>
+      <div class="inline" v-html="store.flagger"></div> <span>VOTE :</span>  {{serie.vote_average}} </li>
     </ul>
 </template>
 
 <style scoped>
 span{
   color: red;
+}
+
+.inline{
+  display: inline;
+}
+
+img{
+  width: 5%;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,10 +1,14 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-    movieList: [],
-    apiURL: 'https://api.themoviedb.org/3/search/movie?api_key=70b5d4fb18cfb09b8168cc33b8d445d7',
+    movieList: [], //Array film
+    seriesList: [], //Array series
+    apiURL: 'https://api.themoviedb.org/3/search/movie?api_key=70b5d4fb18cfb09b8168cc33b8d445d7', // Url Film + Key
+    urlSeries: 'https://api.themoviedb.org/3/search/tv?api_key=70b5d4fb18cfb09b8168cc33b8d445d7', // Url Series + Key
     loading: false,
     searchText: '',
-    apiNameParam: '&query='
+    apiNameParam: '&query=',
+    flagConverter: [],
+    flagger: '<i class="fa-solid fa-flag"></i>'
 }); 
 
